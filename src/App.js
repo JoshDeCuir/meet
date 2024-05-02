9d410279f3b498395afc9be76ae5f73e60f7a6d8
JoshDeCuir <joshmdecuir@gmail.com>
2024-05-02 17:14:56 -0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -40,7 +40,7 @@ const App = () => {
       <div className='alerts-container'>
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null }
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null }
-        {errorAlert.length ? <warningAlert text={warningAlert} /> : null }
+        {errorAlert.length ? <WarningAlert text={warningAlert} /> : null }
       </div>
       < CitySearch 
           allLocations={allLocations} 
