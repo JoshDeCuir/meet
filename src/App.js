097ae3d948f3f48622a2943332de8733377f1002
JoshDeCuir <joshmdecuir@gmail.com>
2024-05-04 18:45:17 -0500

    CityEventsChart created

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import { useEffect, useState } from 'react';
 import { extractLocations, getEvents } from './api';
 import './App.css';
 import { InfoAlert, ErrorAlert, WarningAlert } from './components/Alert';
+import CityEventsChart from './components/CityEventsChart';
 
 
 const App = () => {
@@ -37,6 +38,7 @@ const App = () => {
 
   return (
     <div className="App">
+      <h1>Meet App</h1>
       <div className='alerts-container'>
         {infoAlert.length ? <InfoAlert text={infoAlert}/> : null }
         {errorAlert.length ? <ErrorAlert text={errorAlert} /> : null }
@@ -47,6 +49,7 @@ const App = () => {
           setCurrentCity={setCurrentCity}
           setInfoAlert={setInfoAlert}/>
       < NumberOfEvents setCurrentNOE={setCurrentNOE} setErrorAlert={setErrorAlert}/>
+      < CityEventsChart allLocations={allLocations} events={events} />
       < EventList events={events} />
     </div>
   );
